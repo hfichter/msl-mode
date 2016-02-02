cff8dc1391b0d77ba6cdbd6d20001c6114c56044
Hugo Fichter <hugo@HugoMBP.local>
2016-02-03 02:18:54 +0800

    Inherit from prog-mode instead of fundamental-mode. Remove useless mode variables. Use defvar instead of setq for useful mode variables. Set defvar outside of mode initialization block. Modify syntax entry table instead of creating a new one.

--- msl-mode.el
+++ msl-mode.el
@@ -1,8 +1,9 @@
 ;;; msl-mode.el --- sample major mode for editing MSL
 
 ;; Author: Hugo Fichter 
-;; Version: 1.0
+;; Version: 1.1
 ;; Created: Jan 2016
+;; Last modified: Feb 2016
 ;; Keywords: languages
 ;; Homepage: https://github.com/hfichter/msl-mode
 
@@ -13,96 +14,88 @@
 ;;; Code:
 
 ;; Define several category of keywords
-(setq msl-keywords '("IF" "THEN" "ELSE" "WHILE"))
-(setq msl-constants '("TRUE" "FALSE"))
-(setq msl-boolean-operators '("NOT" "AND" "OR"))
+(defvar msl-mode-keywords '("IF" "THEN" "ELSE" "WHILE")
+  "MSL language syntax keywords")
+(defvar msl-mode-constants '("TRUE" "FALSE")
+  "MSL language constants (booleans)")
+(defvar msl-mode-boolean-operators '("NOT" "AND" "OR")
+  "MSL language boolean operators")
 
 ;; TODO: load funciton names in this version
 ;; (load-file "msl-functions.el")
 
 ;; generate regex string for each category of keywords
-(setq msl-keywords-regexp (regexp-opt msl-keywords 'words))
-(setq msl-constant-regexp (regexp-opt msl-constants 'words))
-(setq msl-boolean-operators-regexp (regexp-opt msl-boolean-operators 'words))
+(setq msl-keywords-regexp (regexp-opt msl-mode-keywords 'words))
+(setq msl-constant-regexp (regexp-opt msl-mode-constants 'words))
+(setq msl-boolean-operators-regexp (regexp-opt msl-mode-boolean-operators 'words))
 ;;(setq msl-functions-regexp (regexp-opt msl-functions 'words))
 
 ;; create the list for font-lock.
 ;; each category of keyword is given a particular face
-(setq msl-font-lock-keywords
+(defvar msl-mode-font-lock-keywords
       `(
         (,msl-constant-regexp .  font-lock-constant-face)
-        (,msl-boolean-operators-regexp .  msl-face-syntax)
+        (,msl-boolean-operators-regexp .  'msl-face-syntax)
 	;; (,msl-functions-regexp . font-lock-function-name-face)
-        (,msl-keywords-regexp . msl-face-keywords)
+        (,msl-keywords-regexp . 'msl-face-keywords)
         ))
-;; set syntax table to detect C and C++ styles comments
-(defvar msl-mode-syntax-table
-  (let ((table (make-syntax-table)))
-    (modify-syntax-entry ?/ ". 14" table)
-    (modify-syntax-entry ?* ". 23" table)
-    (modify-syntax-entry ?/ ". 124b" table)
-    (modify-syntax-entry ?\n "> b" table)
-    (modify-syntax-entry ?\r "> b" table)
-    table)
-  "Syntax table in use in `msl-mode' buffers.")
+
+;; Load MSL custom faces
+(defface msl-face-syntax
+  '((((class color))
+     :foreground "red1"))
+  "Face used for MSL syntax: := , {} () "
+  :group 'msl-faces)
+(defface msl-face-comment
+  '((((class color))
+     :foreground "green3"))
+  "Face used for MSL comments"
+  :group 'msl-faces)
+(defface msl-face-string
+  '((((class color))
+     :foreground "DeepSkyBlue4"
+     :weight bold))
+  "Face used for MSL strings"
+  :group 'msl-faces)
+(defface msl-face-keywords
+  '((((class color))
+     :foreground "blue1"
+     :weight bold))
+  "Face used for MSL keywords"
+  :group 'msl-faces)
 
 ;;;###autoload
-(define-derived-mode msl-mode fundamental-mode
+(define-derived-mode msl-mode prog-mode
   "MSL mode"
-  "Major mode for editing MSL (Murex Scripting Language)"
-  ;; Load MSL custom faces
-  (defvar msl-face-syntax 'msl-face-syntax)
-  (defvar msl-face-comment 'msl-face-comment)
-  (defvar msl-face-string 'msl-face-string)
-  (defvar msl-face-keywords 'msl-face-keywords)
-  (defface msl-face-syntax
-    '((((class color))
-       :foreground "red1"))
-    "Face used for MSL syntax: := , {} () "
-    :group 'msl-faces)
-  (defface msl-face-comment
-    '((((class color))
-       :foreground "green3"))
-    "Face used for MSL comments"
-    :group 'msl-faces)
-  (defface msl-face-string
-    '((((class color))
-       :foreground "DeepSkyBlue4"
-       :weight bold))
-    "Face used for MSL strings"
-    :group 'msl-faces)
-  (defface msl-face-keywords
-    '((((class color))
-       :foreground "blue1"
-       :weight bold))
-    "Face used for MSL keywords"
-    :group 'msl-faces)
+  "Major mode for editing Murex Scripting Language files"
   ;; set  MSL syntax table
   ;; to detect C and C++ style comments
-  (set-syntax-table msl-mode-syntax-table)
+;;  (set-syntax-table msl-mode-syntax-table)
+  (modify-syntax-entry ?/ ". 14" msl-mode-syntax-table)
+  (modify-syntax-entry ?* ". 23" msl-mode-syntax-table)
+  (modify-syntax-entry ?/ ". 124b" msl-mode-syntax-table)
+  (modify-syntax-entry ?\n "> b" msl-mode-syntax-table)
+  (modify-syntax-entry ?\r "> b" msl-mode-syntax-table)
   ;; set face for comments and strings
   (make-local-variable 'font-lock-comment-face)
   (make-local-variable 'font-lock-string-face)
-  (setq font-lock-comment-face msl-face-comment)
-  (setq font-lock-string-face msl-face-string)
+  (setq font-lock-comment-face 'msl-face-comment)
+  (setq font-lock-string-face 'msl-face-string)
   ;; code for syntax highlighting for MSL keywords and const
-  (setq font-lock-defaults '((msl-font-lock-keywords)))
+  (setq font-lock-defaults '((msl-mode-font-lock-keywords)))
   ;; additonal keywords for MSL separators 
   (font-lock-add-keywords nil
 			  ;;(regexp-opt '("," ":" "=" "{" "}" "(" ")"))
-			  '(("[(),:={}]" . msl-face-syntax)))
+			  '(("[(),:={}]" . 'msl-face-syntax)))
   ;; Default MSL mode bindings
   (local-set-key (kbd "C-c f")  'msl-current-function-name))
 
-;; clear memory. no longer needed
-(setq msl-keywords nil)
-(setq msl-constants nil)
 ;;(setq msl-functions nil)
 
-;; clear memory. no longer needed
-(setq msl-keywords-regexp nil)
-(setq msl-constants-regexp nil)
-;;(setq msl-functions-regexp nil)
+;; clear memory. no longer needed, regexp
+(makunbound 'msl-keywords-regexp)
+(makunbound 'msl-boolean-operators-regexp)
+(makunbound 'msl-constant-regexp)
 
 ;; add the mode to the `features' list
 (provide 'msl-mode)
